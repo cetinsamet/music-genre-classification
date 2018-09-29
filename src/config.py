7e994acdf7e67b17cb73ad6cc6f74b3abfb5fc1b
Samet Çetin <e2036432@ceng.metu.edu.tr>
2018-09-29 13:49:21 +0300

    create configuration file

---
+++ src/config.py
@@ -0,0 +1,14 @@
+# 10 GENRES USED FOR CLASSIFICATION
+GENRES      = [ 'blues',
+                'classical',
+                'country',
+                'disco',
+                'hiphop',
+                'jazz',
+                'metal',
+                'pop',
+                'reggae',
+                'rock'  ]
+
+# DEFINE PATHS
+DATAPATH    =   '../data/'
